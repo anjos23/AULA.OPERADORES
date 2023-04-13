804b092833c2b69b50c8db442bc1a5b63b4af7c6
aline dos anjos <alinepinto.2021@gmail.com>
2023-04-12 21:46:39 -0300

    exercicios de operadores

---
+++ index.js
@@ -0,0 +1,28 @@
+// calcule e mostre o valor de 77 F EM k
+let grausFahrenheit = parseFloat(prompt('digite a temperatura Fahrenheit'))
+let grausKelvin = (grausFahrenheit-32)*(5/9) + 273.15;
+
+//console.log(grausFahrenheit, grausKelvin);
+
+
+//calcule e mostre o valor de 80c em F
+ grausFahrenheit = (80)*(9/5) + 32;
+ console.log(grausFahrenheit);
+
+
+
+ //calcule e mostre o valor de 30c em f e K
+
+ grausFahrenheit = (30)*(9/5) + 32;
+ console.log(grausFahrenheit);
+
+
+ grausFahrenheit = parseFloat(prompt('digite a temperatura Fahrenheit'))
+ grausKelvin = (grausFahrenheit-32)*(5/9) + 273.15;
+
+
+
+ //altere o ultimo item  para que o usuario insira o valor  em graus celsius que ele deseja converter 
+    
+
+
