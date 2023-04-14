49681abeac6830775a29599734a74b1fbda6be89
aline dos anjos <alinepinto.2021@gmail.com>
2023-04-13 21:52:17 -0300

    aula 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// calcule e mostre o valor de 77 F EM k
+   /* calcule e mostre o valor de 77 F EM k
 let grausFahrenheit = parseFloat(prompt('digite a temperatura Fahrenheit'))
 let grausKelvin = (grausFahrenheit-32)*(5/9) + 273.15;
 
@@ -11,6 +11,7 @@ let grausKelvin = (grausFahrenheit-32)*(5/9) + 273.15;
 
 
 
+
  //calcule e mostre o valor de 30c em f e K
 
  grausFahrenheit = (30)*(9/5) + 32;
@@ -19,10 +20,70 @@ let grausKelvin = (grausFahrenheit-32)*(5/9) + 273.15;
 
  grausFahrenheit = parseFloat(prompt('digite a temperatura Fahrenheit'))
  grausKelvin = (grausFahrenheit-32)*(5/9) + 273.15;
-
+*/
 
 
  //altere o ultimo item  para que o usuario insira o valor  em graus celsius que ele deseja converter 
     
+
+// solicitar o nome do usuario ,3 notas, calcular a media e se a media for maior ou igual a 7 'dar os parabens ,se nao pedir para repetir o ano 
+
+
+let nomeIformado = prompt ('informe seu nome');//variavel nome informado recebe atribuicao do nome definido pelo usuario
+console.log (nomeIformado) //console.log (imprime informação no console)
+
+let nota1 = parseFloat(prompt('insira primeira nota')); // variavel de nota 
+console.log (nota1);
+
+let nota2 = parseFloat(prompt('insira segunda nota'));
+console.log (nota2);
+
+
+let nota3 = parseFloat(prompt('insira terceira nota'));
+console.log (nota3);
+
+
+console.log(nota1 + nota2 + nota3);
+
+ = (nota1 + nota2 + nota3)/3;
+console.log (nomeIformado, ' ',  media >= 6); //informando nome e media 
+
+
+if (media >= 6){
+   alerte (nomeIformado,'sua media foi ',media, '.Parabens ' );
+    else 
+ { nomeIformado, 'volte no proximo ano '}
+}
+
+//solicitar numero 
+let digiteUmNumero = prompt( 'digiteUmNumero' )
+
+
+//validar se o numero e par
+
+if (digiteUmNumero % 2 === 0) {
+    alert('E PAR')
+   
+}    else { 
+    alert ('e IMPAR')
+   }
+
+
+   //SOLICITAR 2 NUMEROS PARA USUARIO EXIBIR O MAIOR
+
+
+
+let numero1= (prompt('digite primeiro numero'));
+let numero2= (prompt('digite segundo numero'));
+
+
+
+if ( numero1 > numero2 ) {
+    alert(numero1 + ' e maior')
+} else { (numero2 + ' e maior')
+
+}
+ 
+
 
 
